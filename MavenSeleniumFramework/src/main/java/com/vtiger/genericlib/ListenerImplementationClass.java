27104153f293bb9419e2313cdb678af151acbb31
zeliya <zeliya@LAPTOP-KVKSTATV>
2019-01-06 22:18:31 +0530

    second commit

---
+++ MavenSeleniumFramework/src/main/java/com/vtiger/genericlib/ListenerImplementationClass.java
@@ -0,0 +1,62 @@
+package com.vtiger.genericlib;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenerImplementationClass implements ITestListener
+{
+	public void onTestFailure(ITestResult r)
+	{
+		String failedTestName = r.getMethod().getMethodName();
+		EventFiringWebDriver eDriver = new EventFiringWebDriver(BaseClass.driver);
+		File src = eDriver.getScreenshotAs(OutputType.FILE);
+		File dst = new File("./Screenshot/"+failedTestName+".png");
+		try
+		{
+			FileUtils.copyFile(src, dst);
+		}
+		catch(IOException e)
+		{
+			
+		}
+		
+	}
+
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
